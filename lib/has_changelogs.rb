7b75a7dd025a2445f304dc542dcf0e96d237cbc1
Christoph Beck <cbeck@bitcrowd.net>
2015-10-02 17:41:35 +0200

    better option handling

--- lib/has_changelogs.rb
+++ lib/has_changelogs.rb
@@ -15,8 +15,8 @@ module HasChangelogs
       self.has_changelog_options = options.dup
 
 
-       has_changelog_options[:ignore] = (([has_changelog_options[:ignore]].flatten.compact || []) | [:updated_at]).map &:to_s
-       has_changelog_options[:only]   = ([has_changelog_options[:only]].flatten.compact   || [])   .map &:to_s
+       has_changelog_options[:ignore] = (Array(has_changelog_options[:ignore]) | [:updated_at] ).map &:to_s
+       has_changelog_options[:only]   = Array(has_changelog_options[:only]).map &:to_s
 
       # [:ignore, :only].each do |k|
       #     has_changelog_options[k] =
