b24a48ad307121ebbe56ec85f1bc752e670f05d6
elsapet <ebraae@gmail.com>
2015-10-02 17:44:38 +0200

    Refactoring existing specs

--- spec/has_changelogs_spec.rb
+++ spec/has_changelogs_spec.rb
@@ -9,17 +9,17 @@ describe HasChangelogs do
   describe 'options[:only]' do
     subject { OnlyName.create({name: "Lisa Simpson", email: "lisa@simpson.com", uuid: "123ABC"}) }
     context 'name has changed' do
-      it 'logs changes' do
+      it 'is relevant' do
         subject.name = "Ms L Simpson"
-        expect(subject).to receive(:log_changes)
+        expect(subject).to receive(:change_relevant?).and_return(true)
         subject.save
       end
     end
 
     context 'name has not changed' do
-      it 'does not log changes' do
+      it 'is not relevant' do
         subject.email = "lsimpson@springfield.com"
-        expect(subject).to_not receive(:log_changes)
+        expect(subject).to receive(:change_relevant?).and_return(false)
         subject.save
       end
     end
@@ -37,23 +37,52 @@ describe HasChangelogs do
     end
   end
 
-  describe "callbacks" do
+  describe 'callbacks' do
+    
+    context 'change is relevant' do
+      subject = User.create({ name: "Lisa Simpson", email: "lisa@simpson.com", uuid: "123ABC" })
+      before(:each) do
+        allow(subject).to receive(:change_relevant?).and_return(true)
+      end
+      
+      it "should run after_create" do
+        subject = User.new({ name: "Marge Simpson", email: "marge@simpson.com", uuid: "123ABC" })
+        expect(subject).to receive(:record_created)
+        subject.save
+      end
 
+      it "should run before_update" do
+        expect(subject).to receive(:record_updated)
+        subject.save
+      end
 
-    it "should run after create o matter if the change is relevant or not" do
-      subject =  IgnoreName.new({name: "Lisa Simpson", email: "lisa@simpson.com", uuid: "123ABC"})
-      allow(subject).to receive(:change_relevant?).and_return(false)
-      expect(subject).to receive(:record_created)
-      subject.save
+      it "should run after_destroy" do
+        expect(subject).to receive(:record_will_be_destroyed)
+        subject.destroy
+      end
     end
 
-    it "should not run after update if change is not relevant" do
-      subject =  IgnoreName.create({name: "Lisa Simpson", email: "lisa@simpson.com", uuid: "123ABC"})
-      allow(subject).to receive(:change_relevant?).and_return(false)
-      expect(subject).to_not receive(:record_updated)
-      subject.save
-    end
+    context 'change is not relevant' do
+      subject = User.create({ name: "Lisa Simpson", email: "lisa@simpson.com", uuid: "123ABC" })
+      before(:each) do
+        allow(subject).to receive(:change_relevant?).and_return(false)
+      end
+      
+      it "should run after_create" do
+        subject = User.new({ name: "Marge Simpson", email: "marge@simpson.com", uuid: "123ABC" })
+        expect(subject).to receive(:record_created)
+        subject.save
+      end
 
+      it "should not run before_update" do
+        expect(subject).to_not receive(:record_updated)
+        subject.save
+      end
+
+      it "should run after_destroy" do
+        expect(subject).to receive(:record_will_be_destroyed)
+        subject.destroy
+      end
+    end
   end
-
 end
