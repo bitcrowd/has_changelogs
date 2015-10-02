c18f25c2ebbfca6f2cdb6b68fb702dda94e2aa41
Christoph Beck <chris@Christophs-MBP.fritz.box>
2015-10-02 17:24:39 +0200

    added tests

--- spec/has_changelogs_spec.rb
+++ spec/has_changelogs_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 describe HasChangelogs do
 
-  
+
   it 'has a version number' do
     expect(HasChangelogs::VERSION).not_to be nil
   end
 
   describe 'options[:only]' do
     subject { OnlyName.create({name: "Lisa Simpson", email: "lisa@simpson.com", uuid: "123ABC"}) }
-  
+
     context 'name has changed' do
       it 'logs changes' do
         subject.name = "Ms L Simpson"
@@ -29,17 +29,34 @@ describe HasChangelogs do
 
   describe 'options[:ignore]' do
     subject { IgnoreName.create({name: "Lisa Simpson", email: "lisa@simpson.com", uuid: "123ABC"}) }
-  
+
     context 'only name has changed' do
       it 'does not log changes' do
         subject.name = "Ms L Simpson"
+        #allow(subject).to receive(:change_relevant?).and_return(false)
         expect(subject).to_not receive(:log_changes)
         subject.save
       end
     end
   end
 
-  # describe 'options[:if]' do
-  #   subject { IfTrue.create({name: }) }
-  # end
+  describe "callbacks" do
+
+
+    it "should run after create o matter if the change is relevant or not" do
+      subject =  IgnoreName.new({name: "Lisa Simpson", email: "lisa@simpson.com", uuid: "123ABC"})
+      allow(subject).to receive(:change_relevant?).and_return(false)
+      expect(subject).to receive(:record_created)
+      subject.save
+    end
+
+    it "should not run after update if change is not relevant" do
+      subject =  IgnoreName.create({name: "Lisa Simpson", email: "lisa@simpson.com", uuid: "123ABC"})
+      allow(subject).to receive(:change_relevant?).and_return(false)
+      expect(subject).to_not receive(:record_updated)
+      subject.save
+    end
+
+  end
+
 end
